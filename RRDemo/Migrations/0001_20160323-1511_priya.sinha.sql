aa8c69dd6cc34b20504f5a28800ecefbb694e6c6
Priya Sinha <priya.sinha@red-gate.com>
2016-03-23 15:19:38 +0000

    Initial baseline database

---
+++ RRDemo/Migrations/0001_20160323-1511_priya.sinha.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="e8a06fa4-a295-47ac-80bc-88a2b4a9ac40" />
+GO
+
+PRINT N'Creating [dbo].[Table_1]'
+GO
+CREATE TABLE [dbo].[Table_1]
+(
+[Id] [int] NULL,
+[Name] [varchar] (50) NULL
+)
+GO
---
+++ RRDemo/Schema-Model/dbo/Tables/Table_1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_1]
+(
+[Id] [int] NULL,
+[Name] [varchar] (50) NULL
+)
+GO
