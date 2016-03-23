eb44c165e414a66147bcd0f04ac60c52afb222e0
Priya Sinha <priya.sinha@red-gate.com>
2016-03-23 15:23:18 +0000

    Added Table_2

---
+++ RRDemo/Migrations/0002_20160323-1522_priya.sinha.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="7c2c0e36-5bb5-46d4-9a33-1b06dc65a98d" />
+GO
+
+PRINT N'Creating [dbo].[Table_2]'
+GO
+CREATE TABLE [dbo].[Table_2]
+(
+[id2] [int] NULL,
+[name2] [varchar] (50) NULL
+)
+GO
