2a2672fcf058f73359ed8fd47fddb76f701ccd7e
Priya Sinha <priya.sinha@red-gate.com>
2016-03-24 11:29:45 +0000

    Adding Solution to GIT

---
+++ RRDemo/Schema-Model/dbo/Tables/Table_2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_2]
+(
+[id2] [int] NULL,
+[name2] [varchar] (50) NULL
+)
+GO
---
+++ RRDemo/Schema-Model/dbo/Tables/Table_3.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Table_3]
+(
+[id3] [int] NULL,
+[name] [varchar] (50) NULL
+)
+GO
