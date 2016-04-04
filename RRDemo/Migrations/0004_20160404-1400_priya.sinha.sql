9974664278d9bfd0c3ca55c8c8f8361b93666a11
Priya Sinha <priya.sinha@red-gate.com>
2016-04-04 14:02:04 +0100

    Added Table_4

---
+++ RRDemo/Migrations/0004_20160404-1400_priya.sinha.sql
@@ -0,0 +1,11 @@
+﻿-- <Migration ID="b5711edc-180c-4904-909a-93b8fd16d486" />
+GO
+
+PRINT N'Creating [dbo].[Table_4]'
+GO
+CREATE TABLE [dbo].[Table_4]
+(
+[id] [int] NULL,
+[Name] [nchar] (10) NULL
+)
+GO
